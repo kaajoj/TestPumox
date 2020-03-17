f7b8a8527bad59e0e8203e476beb031692b2adf2
kaajoj <kaajooj@gmail.com>
2020-03-17 18:34:39 +0100

    Init repo, models creation

---
+++ Models/Companies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPumox.Models
+{
+    public class Companies
+    {
+        public Int64 CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public int FoundationYear { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPumox.Models
+{
+    public enum PositionName
+    {
+        Administrator,
+        Developer,
+        Architect,
+        Manager
+    }
+
+    public class Employee
+    {
+        public Int64 EmployeeId { get; set; }
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        public DateTime BirthdayDate { get; set; }
+
+        public PositionName Name { get; set; }
+
+        public Int64 CompanyId { get; set; }
+    }
+}
